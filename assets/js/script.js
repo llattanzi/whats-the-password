c3cde7312904cbf3a6201fedfe042f79612791f3
llattanzi <lauren71442@gmail.com>
2022-03-05 14:21:44 -0500

    add prompt for password length and validate response

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,40 @@
 // Assignment code here
+// prompts include:
+// 1. length of password (between 8 and 128 characters)
+// 2. lowercase?
+// 3. uppercase?
+// 4. numeric?
+// 5. special characters?
+// validate responses, atleast 1 character type should be selected
 
+// prompt the user for password length and validate the response
+function getCharacterLength() {
+  var passwordLength = window.prompt("Please type the desired length of your password (must be between 8 and 128 characters long).");
+  passwordLength = parseInt(passwordLength);
+  if (passwordLength) {
+    if (passwordLength < 8 || passwordLength > 128) {
+      window.alert("Password length must be between 8 and 128 characters long.");
+      return getCharacterLength();
+    }
+  }
+  else {
+    window.alert("You must enter a numeric value.")
+    return getCharacterLength();
+  }
+  return passwordLength;
+}
+
+function getPasswordInfo() {
+  var passwordInfo = {
+    characterLength: getCharacterLength(),
+
+  }
+
+}
+
+function generatePassword() {
+  var passwordInfo = getPasswordInfo();
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
